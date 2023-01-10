77888c618371d4b377ee34dab4b899ff56e913b2
esp32-urobot <urobot2011@gmail.com>
2023-01-10 21:42:13 +0900

    Kalimba layout to piano function added
    Signed-off-by: esp32-urobot <urobot2011@gmail.com>

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ var instrument;
 var loadFile, loadDataUri, Player;
 var AudioContext = window.AudioContext || window.webkitAudioContext || false;
 var ac = new AudioContext || new webkitAudioContext;
+var scalekeys = ["C0", "C#0", "Db0", "D0", "D#0", "Eb0", "E0", "F0", "F#0", "Gb0", "G0", "G#0", "Ab0", "A0", "A#0", "Bb0", "B0", "C1", "C#1", "Db1", "D1", "D#1", "Eb1", "E1", "F1", "F#1", "Gb1", "G1", "G#1", "Ab1", "A1", "A#1", "Bb1", "B1", "C2", "C#2", "Db2", "D2", "D#2", "Eb2", "E2", "F2", "F#2", "Gb2", "G2", "G#2", "Ab2", "A2", "A#2", "Bb2", "B2", "C3", "C#3", "Db3", "D3", "D#3", "Eb3", "E3", "F3", "F#3", "Gb3", "G3", "G#3", "Ab3", "A3", "A#3", "Bb3", "B3", "C4", "C#4", "Db4", "D4", "D#4", "Eb4", "E4", "F4", "F#4", "Gb4", "G4", "G#4", "Ab4", "A4", "A#4", "Bb4", "B4", "C5", "C#5", "Db5", "D5", "D#5", "Eb5", "E5", "F5", "F#5", "Gb5", "G5", "G#5", "Ab5", "A5", "A#5", "Bb5", "B5", "C6", "C#6", "Db6", "D6", "D#6", "Eb6", "E6", "F6", "F#6", "Gb6", "G6", "G#6", "Ab6", "A6", "A#6", "Bb6", "B6", "C7", "C#7", "Db7", "D7", "D#7", "Eb7", "E7", "F7", "F#7", "Gb7", "G7", "G#7", "Ab7", "A7", "A#7", "Bb7", "B7", "C8"];
 var currentTime;
 var timeoutId;
 var timeoutId2;
 var timeoutId3;
 var bool1 = 0;
 var tempo = 50;
+var keynotes = scalekeys;
+var notelengths = [];
+
+var kalimbadiv = document.querySelector('#kalimba');
+
 
 var tempodiv = document.querySelector('.tempo');
 var tempodisplaydiv = document.querySelector('#tempo-display');
@@ -25,6 +31,7 @@ var tempobuttondiv = document.querySelector('.tempo');
 
 var shownumbersbuttondiv = document.querySelector('#show_numbers');
 var keyboardlayoutbuttondiv = document.querySelector('#keyboard_layout');
+var kalimbalayoutbuttondiv = document.querySelector('#kalimba_layout');
 
 window.addEventListener("keydown", function (e) {
 	var key = notetonote(keyboardtonote(e.key));
@@ -64,6 +71,21 @@ var notetonote = function(note) {
     	var notes1 = ["D6",  "B5",  "G5",  "E5",  "C5",  "A4",  "F4",  "D4",  "C4",  "E4",  "G4",  "B4",  "D5",  "F5",  "A5",  "C6",  "E6"];
 	return notes1[notes.indexOf(note)];
 }
+
+var keyToNote = [];
+var noteToKey = [];
+(function() {
+	//first note : 0x15 == 33
+	//last note : 0x6C == 264
+  var keys = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
+  var keys2 = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+	for (var i = 33; i <= 264; i++) {
+		var octave = (i - 12) / 12 >> 0;
+		var name = keys2[i % 12] + octave;
+		keyToNote[name] = i;
+		noteToKey[i] = name;
+	}
+})();
 
 var changeTempo = function(tempo) {
 	Player.tempo = tempo;
@@ -121,6 +143,73 @@ tempobuttondiv.addEventListener("change", function(e) {
 	document.getElementById('tempo-display').innerHTML = this.value;
 });
 
+kalimbalayoutbuttondiv.addEventListener("click", function(e) {
+	console.log(e);
+	if(kalimbalayoutbuttondiv.checked) {
+		keyboardlayoutbuttondiv.checked = 1;
+		kalimbadiv.innerHTML = `
+					<!--<kalimba-key>-->
+				<div class="key" data-note="C4"></div>
+				<div class="key" data-note="D4"></div>
+				<div class="key" data-note="E4"></div>
+				<div class="key" data-note="F4"></div>
+				<div class="key" data-note="G4"></div>
+				<div class="key" data-note="A4"></div>
+				<div class="key" data-note="B4"></div>
+				<div class="key" data-note="C5"></div>
+				<div class="key" data-note="D5"></div>
+				<div class="key" data-note="E5"></div>
+				<div class="key" data-note="F5"></div>
+				<div class="key" data-note="G5"></div>
+				<div class="key" data-note="A5"></div>
+				<div class="key" data-note="B5"></div>
+				<div class="key " data-note="C6"></div>
+				<div class="key" data-note="D6"></div>
+				<div class="key last-key" data-note="E6"></div>
+			<!--</kalimba-key>-->
+			<bottom-pad></bottom-pad>
+			<top-pad></top-pad>
+			<bottom-pad class="last-pad"></bottom-pad>
+			<div class="logo">Kalimba sheet music writer<br>1.0.3</div>
+		`;
+		keydivs = document.querySelectorAll('.key');
+		keydivs.forEach((key) => {
+	key.innerHTML = '<span class="key-text">' + key.getAttribute("data-note").substring(0, 1) + '<br>' + notetonumber(key.getAttribute("data-note")) + '</span>';
+});
+	} else {
+		keyboardlayoutbuttondiv.checked = 0;
+		kalimbadiv.innerHTML = `
+					<!--<kalimba-key>-->
+				<div class="key" data-note="D6"></div>
+				<div class="key" data-note="B5"></div>
+				<div class="key" data-note="G5"></div>
+				<div class="key" data-note="E5"></div>
+				<div class="key" data-note="C5"></div>
+				<div class="key" data-note="A4"></div>
+				<div class="key" data-note="F4"></div>
+				<div class="key" data-note="D4"></div>
+				<div class="key" data-note="C4"></div>
+				<div class="key" data-note="E4"></div>
+				<div class="key" data-note="G4"></div>
+				<div class="key" data-note="B4"></div>
+				<div class="key" data-note="D5"></div>
+				<div class="key" data-note="F5"></div>
+				<div class="key " data-note="A5"></div>
+				<div class="key" data-note="C6"></div>
+				<div class="key last-key" data-note="E6"></div>
+			<!--</kalimba-key>-->
+			<bottom-pad></bottom-pad>
+			<top-pad></top-pad>
+			<bottom-pad class="last-pad"></bottom-pad>
+			<div class="logo">Kalimba sheet music writer<br>1.0.3</div>
+		`;
+		keydivs = document.querySelectorAll('.key');
+		keydivs.forEach((key) => {
+	key.innerHTML = '<span class="key-text">' + key.getAttribute("data-note").substring(0, 1) + '<br>' + notetonumber(key.getAttribute("data-note")) + '</span>';
+});
+	}
+});
+
 var playmidi = function(event) {
 	if (bool1 == 0) {
 		tempo = Player.tempo;
@@ -129,15 +218,37 @@ var playmidi = function(event) {
 	Player.setTempo(tempodiv.value);
 	if (event.name == 'Note on' && event.velocity > 0) {
 		//$('key-note-animation').append('<div data-key-note="' + event.noteName + '"></div>');
-		if (shownumbersbuttondiv.checked) {
-			keynoteanimationdiv.insertAdjacentHTML("afterbegin", `
-		<div data-key-note="` + event.noteName + `"><span class="key-text">` + notetonumber(event.noteName) + `</span></div>
-		`);
+		if(keynotes[event.noteName] == 1) {
+			keynotes[event.noteName] = 0;
 		} else {
-			keynoteanimationdiv.insertAdjacentHTML("afterbegin", `
-		<div data-key-note="` + event.noteName + `"></div>
-		`);
+			keynotes[event.noteName] = 1;
 		}
+		notelengths[event.noteName] = ac.currentTime;
+		if (shownumbersbuttondiv.checked) {
+			if(kalimbalayoutbuttondiv.checked) {
+				keynoteanimationdiv.insertAdjacentHTML("afterbegin", `<div data-key-note="` + notetonote(event.noteName) + `"><span class="key-text">` + notetonumber(event.noteName) + `</span></div>`);
+			} else {
+				keynoteanimationdiv.insertAdjacentHTML("afterbegin", `<div data-key-note="` + event.noteName + `"><span class="key-text">` + notetonumber(event.noteName) + `</span></div>`);
+			}															
+		} else {
+			if(kalimbalayoutbuttondiv.checked) {
+				keynoteanimationdiv.insertAdjacentHTML("afterbegin", `<div data-key-note="` + notetonote(event.noteName) + `"></div>`);
+			} else {
+				keynoteanimationdiv.insertAdjacentHTML("afterbegin", `<div data-key-note="` + event.noteName + `"></div>`);
+			}
+		}
+	}
+	if (event.name == 'Note off') {
+		//height: 30px;
+		var notediv = document.querySelector('[data-key-note="' + event.noteName + '"]');
+		//notediv.setAttribute('height', notelengths[event.noteName] + "px");
+		console.log("event: notediv, " + notediv);
+		try {
+  		//notediv.setAttribute('style', "height: " + (((ac.currentTime - notelengths[event.noteName])*10) > 50 ? (ac.currentTime - notelengths[event.noteName]) : ((ac.currentTime - notelengths[event.noteName])*10) < 20 ? ((ac.currentTime - notelengths[event.noteName])*30) : ((ac.currentTime - notelengths[event.noteName])*10)) + "px");
+		} catch (error) {
+  		console.error(error);
+		}
+		notelengths[event.noteName] = 0;
 	}
 	setTimeout(function() {
 		if (event.name == 'Note on') {
@@ -184,7 +295,7 @@ var lesson_mode = function(event) {
 }
 
 keydivs.forEach((key) => {
-	key.innerHTML = '<span class="key-text">' + key.getAttribute("data-note").substring(0, 1) + '<br>' + key.getAttribute("data-key") + '</span>';
+	key.innerHTML = '<span class="key-text">' + key.getAttribute("data-note").substring(0, 1) + '<br>' + notetonumber(key.getAttribute("data-note")) + '</span>';
 });
 
 Soundfont.instrument(ac, 'kalimba').then(function(instrumentnow) {
